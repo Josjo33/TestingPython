67038e1c277aca2d2078286c14a12ac2dd02803a
Josjo33 <joshuahugo131@gmail.com>
2025-04-05 12:14:15 -0500

    feat(EsPrimo): agrega función para verificar si un número es primo

---
+++ SESIÓN 4/EsPrimo.py
@@ -0,0 +1,16 @@
+import math
+
+
+def EsPrimo (number):
+    if number <= 1 or (number%2) == 0 :
+        return False
+    for check in range(3, int (math.sqrt(number))):
+        if number % check == 0:
+            return False
+    return True
+
+def check(n):
+    print("EsPrimo(" + str(n) + ") = " + str(EsPrimo(n)))
+
+check(1)
+check(2)
